93bd358240a3a8906445fb378951362cd7ed765a
unknown <lhufancia@gbox.adnu.edu.ph>
2025-10-28 02:27:14 +0800

    added format validation and error handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,17 @@ import Signup from "./pages/auth/Signup";
 import Login from "./pages/auth/Login";
 import OwnerInformation from "./pages/owners/Owner_Information";
 import OwnerSubmission from "./pages/owners/Owner_Submission";
-import "leaflet/dist/leaflet.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Regular routes */}
-        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/owner-information" element={<OwnerInformation />} />
         <Route path="/owner-submission" element={<OwnerSubmission />} />
       </Routes>
+
     </Router>
   );
 }
