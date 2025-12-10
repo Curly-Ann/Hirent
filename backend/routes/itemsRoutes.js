de9b07e105d335c04d76827e33aae55e6cd3a18e
Maria Curly Ann A. Lumibao <mlumibao@gbox.adnu.edu.ph>
2025-12-11 05:06:11 +0800

    delete extra

--- backend/routes/itemsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const itemsController = require('../controllers/itemController');
-
-router.get('/search', itemsController.searchItems);
-router.get('/items', itemsController.getAllItems);
-router.post('/items', itemsController.createItem);                   
-
-module.exports = router;
