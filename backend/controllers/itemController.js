941c3fe4c8f92e63ad8753b1a67ecae7a53b3e6a
Maria Curly Ann A. Lumibao <mlumibao@gbox.adnu.edu.ph>
2025-12-14 23:38:40 +0800

    browse

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -21,18 +21,11 @@ exports.getSingleItem = async (req, res) => {
 // ------------------------
 exports.getAllItems = async (req, res) => {
   try {
-    const items = await Item.find({ status: 'active' });
-
-    res.json({
-      success: true,
-      items,
-    });
-
+    const items = await Item.find();
+    res.status(200).json(items); 
   } catch (err) {
-    res.status(500).json({ 
-      success: false,
-      msg: "Error fetching items" 
-    });
+    console.error("Error fetching items:", err);
+    res.status(500).json({ message: "Error fetching items" });
   }
 };
 // ------------------------
